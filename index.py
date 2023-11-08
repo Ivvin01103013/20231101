fdfc46634faff4b23b19f6ec9fb8df27ecddb756
Ivvin01103013 <o1103013@gm.pu.edu.tw>
2023-11-08 16:18:55 +0800

    make it better

--- index.py
+++ index.py
@@ -69,7 +69,7 @@ def search():
         keyword = request.form["keyword"]
         Result = "您輸入的关键字：" + keyword
 
-        Result += "＜br＞"
+        Result += "<br>"
         db = firestore.client()     
         collection_ref = db.collection("人選之人─造浪者")    
         docs = collection_ref.order_by("birth").get()    
